2ab43371253582e6302840850bc3b356463a3f71
Igor Kushnir <mr.kushnir.igor@gmail.com>
2023-07-18 10:32:29 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,31 +1,74 @@
+// import React, { useEffect } from 'react';
+// import PropTypes from 'prop-types';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+// import styles from './Modal.module.css';
+
+// const Modal = ({ image, onClose }) => {
+//   useEffect(() => {
+//     const lightbox = new SimpleLightbox('.Overlay', {
+//       captionsData: 'alt',
+//     });
+
+//     const handleKeyDown = (event) => {
+//       if (event.code === 'Escape') {
+//         onClose();
+//       }
+//     };
+
+//     lightbox.on('shown.simplelightbox', () => {
+//       window.addEventListener('keydown', handleKeyDown);
+//     });
+
+//     lightbox.on('closed.simplelightbox', () => {
+//       window.removeEventListener('keydown', handleKeyDown);
+//     });
+
+//     return () => {
+//       lightbox.destroy();
+//       window.removeEventListener('keydown', handleKeyDown);
+//     };
+//   }, [onClose]);
+
+//   const handleBackdropClick = (event) => {
+//     if (event.target === event.currentTarget) {
+//       onClose();
+//     }
+//   };
+
+//   return (
+//     <div className={styles.Overlay} onClick={handleBackdropClick}>
+//       <div className={styles.Modal}>
+//         <img src={image.largeImageURL} alt={image.tags} />
+//       </div>
+//     </div>
+//   );
+// };
+
+// Modal.propTypes = {
+//   image: PropTypes.object.isRequired,
+//   onClose: PropTypes.func.isRequired,
+// };
+
+// export default Modal;
+
+
+
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 import styles from './Modal.module.css';
 
 const Modal = ({ image, onClose }) => {
   useEffect(() => {
-    const lightbox = new SimpleLightbox('.Overlay', {
-      captionsData: 'alt',
-    });
-
     const handleKeyDown = (event) => {
       if (event.code === 'Escape') {
         onClose();
       }
     };
 
-    lightbox.on('shown.simplelightbox', () => {
-      window.addEventListener('keydown', handleKeyDown);
-    });
-
-    lightbox.on('closed.simplelightbox', () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    });
+    window.addEventListener('keydown', handleKeyDown);
 
     return () => {
-      lightbox.destroy();
       window.removeEventListener('keydown', handleKeyDown);
     };
   }, [onClose]);
